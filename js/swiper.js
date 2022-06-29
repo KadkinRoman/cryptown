26b3336036b75cb3bd3710b832ddcd63ddde4bae
Roman <kadkin.roman@gmail.com>
2022-06-29 04:29:16 +0300

    feat: add team and footer sections

--- js/swiper.js
+++ js/swiper.js
@@ -10,33 +10,13 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/js/app.js":
-/*!***********************!*\
-  !*** ./src/js/app.js ***!
-  \***********************/
+/***/ "./src/js/swiper.js":
+/*!**************************!*\
+  !*** ./src/js/swiper.js ***!
+  \**************************/
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ \"./src/js/utils/index.js\");\n/* harmony import */ var _modules_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/core */ \"./src/js/modules/core.js\");\n/* harmony import */ var swiper__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! swiper */ \"./node_modules/swiper/swiper.esm.js\");\n // Core\n\n // import \"./modules/swiper\"\n\n\nswiper__WEBPACK_IMPORTED_MODULE_2__[\"default\"].use([swiper__WEBPACK_IMPORTED_MODULE_2__.Navigation, swiper__WEBPACK_IMPORTED_MODULE_2__.Pagination]);\nvar swiper = new swiper__WEBPACK_IMPORTED_MODULE_2__[\"default\"]('.swiper', {\n  // Optional parameters\n  direction: 'vertical',\n  loop: true,\n  // If we need pagination\n  pagination: {\n    el: '.swiper-pagination'\n  },\n  // Navigation arrows\n  navigation: {\n    nextEl: '.swiper-button-next',\n    prevEl: '.swiper-button-prev'\n  },\n  // And if we need scrollbar\n  scrollbar: {\n    el: '.swiper-scrollbar'\n  }\n}); // Custom\n\n$(document).ready(function () {\n  /**\r\n   * Scroll To Slug \r\n   * [data-scroll=\"#id\"]\r\n   */\n  (function () {\n    $(document).on('click', '[data-scroll]', function (e) {\n      (0,_utils__WEBPACK_IMPORTED_MODULE_0__.scrollToSlug)($(this).data('scroll') || $(this).attr('href'));\n    });\n  })();\n});\n\n//# sourceURL=webpack://html_start_boilerplate/./src/js/app.js?");
-
-/***/ }),
-
-/***/ "./src/js/modules/core.js":
-/*!********************************!*\
-  !*** ./src/js/modules/core.js ***!
-  \********************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../utils */ \"./src/js/utils/index.js\");\n // Global variables\n\nwindow.screenWidth = 0;\n/**\r\n * Set vh as custom CSS property (fix for iOS)\r\n */\n\nvar setVhCustomProperty = function setVhCustomProperty() {\n  var vh = window.innerHeight * 0.01;\n  document.documentElement.style.setProperty('--vh', vh + 'px');\n};\n/**\r\n * Set screen width to global\r\n */\n\n\nvar setGlobalScreenWidth = function setGlobalScreenWidth() {\n  window.screenWidth = $(window).width();\n};\n/**\r\n * Set client platform classes to <html/>\r\n */\n\n\nvar setClientPlatformClasses = function setClientPlatformClasses() {\n  // MacOS\n  if ((0,_utils__WEBPACK_IMPORTED_MODULE_0__.isMac)()) {\n    $('body').addClass('is-mac');\n  } // iOS\n\n\n  if ((0,_utils__WEBPACK_IMPORTED_MODULE_0__.isIos)()) {\n    $('body').addClass('is-ios');\n  }\n};\n\n(function () {\n  // Custom vh property\n  setVhCustomProperty();\n  $(window).on('resize', setVhCustomProperty); // Screen width\n\n  setGlobalScreenWidth();\n  $(window).on('resize', setGlobalScreenWidth); // Detect Client\n\n  setClientPlatformClasses();\n})();\n\n//# sourceURL=webpack://html_start_boilerplate/./src/js/modules/core.js?");
-
-/***/ }),
-
-/***/ "./src/js/utils/index.js":
-/*!*******************************!*\
-  !*** ./src/js/utils/index.js ***!
-  \*******************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"isIos\": function() { return /* binding */ isIos; },\n/* harmony export */   \"isMac\": function() { return /* binding */ isMac; },\n/* harmony export */   \"scrollToSlug\": function() { return /* binding */ scrollToSlug; }\n/* harmony export */ });\n/**\r\n * Scroll To Slug\r\n * @param {'block ID'} id \r\n */\nvar scrollToSlug = function scrollToSlug(id) {\n  if ($(id).length) {\n    var headerOffset = $('.header').outerHeight() + 20;\n    $('html,body').animate({\n      scrollTop: $(id).offset().top - headerOffset\n    }, {\n      duration: 200\n    });\n  }\n};\n/**\r\n * Check if is iOS platform\r\n */\n\nvar isIos = function isIos() {\n  return /iPhone|iPad|iPod/i.test(window.navigator.platform);\n};\n/**\r\n * Check if is Mac platform\r\n */\n\nvar isMac = function isMac() {\n  return /Mac/.test(window.navigator.platform);\n};\n\n//# sourceURL=webpack://html_start_boilerplate/./src/js/utils/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var swiper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! swiper */ \"./node_modules/swiper/swiper.esm.js\");\n\nvar swiper = new swiper__WEBPACK_IMPORTED_MODULE_0__[\"default\"]('.swiper', {\n  // Optional parameters\n  direction: 'vertical',\n  loop: true,\n  // And if we need scrollbar\n  scrollbar: {\n    el: '.swiper-scrollbar'\n  }\n});\n\n//# sourceURL=webpack://html_start_boilerplate/./src/js/swiper.js?");
 
 /***/ }),
 
@@ -1120,7 +1100,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/app.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/swiper.js");
 /******/ 	
 /******/ })()
 ;
